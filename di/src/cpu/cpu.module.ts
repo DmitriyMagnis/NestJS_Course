40cb1d2535b09d051d75afe4be0aa37f013384ad
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2025-03-18 11:27:26 +0100

    ADD/handling nest di container inmport and exports

--- di/src/cpu/cpu.module.ts
+++ di/src/cpu/cpu.module.ts
@@ -3,7 +3,8 @@ import { CpuService } from './cpu.service';
 import { PowerModule } from '../power/power.module';
 
 @Module({
-  providers: [CpuService],
   imports: [PowerModule],
+  exports: [CpuService],
+  providers: [CpuService],
 })
 export class CpuModule {}
--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from '../power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private readonly powerService: PowerService) {}
+
+  compute(a: number, b: number): number {
+    console.log('Sum watt %s', a + b);
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
