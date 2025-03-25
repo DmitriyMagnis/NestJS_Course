bb0da61d1a1b2562dcd67a68f589b9a5b1784b6a
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2025-03-25 19:18:39 +0100

    ADD/serialization decorator

---
+++ mycarprice/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) =>
+        plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
+    );
+  }
+}
