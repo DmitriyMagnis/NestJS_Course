74d2d11608093a5331b6408d8f89d0191a531f5d
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2025-04-10 19:19:12 +0200

    ADD/update reports, estimate creation and report query filter

--- mycarprice/src/reports/dto/report.dto.ts
+++ mycarprice/src/reports/dto/report.dto.ts
@@ -25,6 +25,9 @@ export class ReportDto {
   @Expose()
   lng: number;
 
+  @Expose()
+  approved: boolean;
+
   @Transform(({ obj }) => obj.user.id)
   @Expose()
   userId: number;
--- mycarprice/src/reports/reports.controller.ts
+++ mycarprice/src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -6,6 +15,9 @@ import { CurrentUserDecorator } from 'src/users/decorators/current-user.decorato
 import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dto/report.dto';
+import { ApproveReportDto } from './dto/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -19,5 +31,16 @@ export class ReportsController {
     @CurrentUserDecorator() user: User,
   ) {
     return this.reportsService.create(body, user);
+  }
+
+  @UseGuards(AdminGuard)
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
   }
 }
